3e0881eb32392335eba085f2daed5545f3d3a12f
queenielyk <queenielyk@gmail.com>
2018-07-01 00:03:30 +0800

    db update

--- sqlite_create.py
+++ sqlite_create.py
@@ -2,17 +2,16 @@ import sqlite3
 import csv
 import os.path
 
-'''
-def load_data1(path='./Restaurant.csv'):
+def load_data1(path='./rest.csv'):
     Rest = list()
-    with open('./Restaurant.csv','r') as f:
+    with open('./rest.csv','r') as f:
         for line in f.readlines()[1:]:
             Rest.append(line.rstrip().split(','))
     return Rest
-'''
-def load_data2(path='./menu.csv'):
+
+def load_data2(path='./add.csv'):
     Add = list()
-    with open('./menu.csv','r') as f:
+    with open('./add.csv','r') as f:
         for line in f.readlines()[1:]:
             Add.append(line.rstrip().split(','))
     return Add
@@ -34,31 +33,28 @@ def connect_db(path=''):
     pass
 '''
 
-'''
 def insert_item1(a,b,c,d,e,f,g,h,_cursor):
     command = "INSERT INTO Restaurant (Name, Phone, Style, OT, Facebook, Seat, Payment, Line) \
-            VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', %d, %d, \'%s\');" % (a,b,c,d,e,f,g,h)
+            VALUES (\'%s\', \'%s\', %d, \'%s\', \'%s\', %d, %d, \'%s\');" % (a,b,c,d,e,f,g,h)
     _cursor.execute(command)
 
     pass
-    '''
 
-def insert_item2(a,b,_cursor):
-    command = "INSERT INTO Menu (Restaurant,Style) VALUES (\'%s\', \'%s\');" % (a,b)
+def insert_item2(a,b,c,d,_cursor):
+    command = "INSERT INTO Address (Area, District, Street, RestaurantName) VALUES (\'%s\', \'%s\',\'%s\', \'%s\');" % (a,b,c,d)
     _cursor.execute(command)
 
     pass
 
 
 if __name__=="__main__":
-#    data1 = load_data1()
+    data1 = load_data1()
     data2 = load_data2()
     conn = connect_db()
-    '''
     for items in data1:
-        insert_item1(items[0],items[1],items[2],items[3],items[4],int(items[5]),int(items[6]),items[7], conn.cursor())
-        '''
+        insert_item1(items[0],items[1],int(items[2]),items[3],items[4],int(items[5]),int(items[6]),items[7], conn.cursor())
+
     for items in data2:
-        insert_item2(items[0],items[1],conn.cursor())
+        insert_item2(items[0],items[1],items[2],items[3],conn.cursor())
     conn.commit()
     conn.close()
